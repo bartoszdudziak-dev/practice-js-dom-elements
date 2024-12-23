329ab346e790b808a507bb27a102e68fa033587c
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2024-12-23 17:29:44 +0100

    Task 04 completed

--- 04/app.js
+++ 04/app.js
@@ -1,8 +1,24 @@
-console.log('DOM');
+console.log("DOM");
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+  { text: "start", url: "/" },
+  { text: "galeria", url: "/gallery" },
+  { text: "kontakt", url: "/contact" },
 ];
+
+const menuElement = document.createElement("ul");
+
+menuItems.forEach((item) => {
+  const itemElement = document.createElement("li");
+
+  const linkElement = document.createElement("a");
+  linkElement.setAttribute("href", item.url);
+  linkElement.textContent = item.text;
+
+  itemElement.appendChild(linkElement);
+  menuElement.appendChild(itemElement);
+});
+
+const navigationElement = document.querySelector("nav");
+if (navigationElement) navigationElement.appendChild(menuElement);
