42adaa5535ca5fa7a5eedc2092d375c66847c2f4
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2024-12-23 16:35:10 +0100

    Task 01 completed

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,11 @@
-console.log('DOM');
+console.log("DOM");
+
+const element = document.querySelector(
+  ".comments__item.comments__item--newest"
+);
+
+if (element) {
+  const elementsInfoList = element.querySelectorAll(`*[data-info]`);
+
+  console.log(`Found: ${elementsInfoList.length} elements`);
+}
