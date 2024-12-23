ea92b8ec5ae543aa0d533541b2f176611b81f38f
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2024-12-23 16:40:08 +0100

    Task 02 completed

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,10 @@
-console.log('DOM');
+console.log("DOM");
+
+const linksList = document.querySelectorAll("a[data-url]");
+
+if (linksList) {
+  linksList.forEach((link) => {
+    const href = link.dataset.url;
+    link.setAttribute("href", href);
+  });
+}
